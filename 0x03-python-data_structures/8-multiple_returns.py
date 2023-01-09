54623c0ce28935d389aa3fcc20c6aec53a823417
Abeltadesse19 <tadesseabel19@gmail.com>
2023-01-09 11:12:45 -0800

    task 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if len(sentence) > 0:
-        return tuple([len(sentence), sentence[0]])
-                else:
-                return tuple([0, None])
+    if sentence != '':
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
