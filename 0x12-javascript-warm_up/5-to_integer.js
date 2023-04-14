ceb8fc30d1a93892f9074f62160deac16f361c39
Abeltadesse19 <tadesseabel19@gmail.com>
2023-04-14 12:09:28 -0700

    java task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 
-const num = Math.floor(Number(process.argv[2]));
-const myVar(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const args = process.argv.slice[2];
+const numArg = Math.floor(Number(argv[2]));
+const myVa(isNaN(num) ? 'Not a number' : `My number: ${num}`);
 console.log(myVar);
